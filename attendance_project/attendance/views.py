3adbfd9f45a872ce16da58e7e2b21f8ed03972b0
sam1-khan <samikhan15262822@gmail.com>
2024-11-01 00:22:29 +0500

    Debugging in views.py before celery implementation

--- attendance_project/attendance/views.py
+++ attendance_project/attendance/views.py
@@ -1,8 +1,8 @@
 from .models import Attendance
 from django.utils import timezone
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView
-
+from datetime import timedelta
 class AttendanceListView(OwnerListView):
     model = Attendance
     template_name = "attendance/list.html"
@@ -44,23 +44,23 @@ class AttendanceUpdateView(OwnerUpdateView):
 
 def send_reminder(self):
     current_time = timezone.now()
-    reminder_threshold = current_time - timedelta(minutes=0)
+    reminder_threshold = current_time - timedelta(minutes=1)
     employees_to_remind = Attendance.objects.filter(
         is_checked_out=False,
         check_in__lt=reminder_threshold  # Ensure the check_in is before the reminder threshold
     )
 
-    for attendance in employees_to_remind:
+    for emp in employees_to_remind:
         subject = "Reminder: You have not checked out!"
         message = (
-            f"Hello {self.employee.user.get_full_name() if self.employee.user.first_name else self.employee.user.get_username()},\n\n"
+            f"Hello {emp.employee.user.get_full_name() if emp.employee.user.first_name else emp.employee.user.get_username()},\n\n"
             "This is a reminder that you have not marked your checkout for today. "
             "Please make sure to check out to complete your attendance record.\n\n"
             "Thank you!"
         )
 
         # Send email
-        self.employee.user.email_user(
+        emp.employee.user.email_user(
             subject,
             message
         )
