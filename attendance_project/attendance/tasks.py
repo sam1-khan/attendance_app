9ceeab2bf5474759d9589c7854d30b48afa5ebbc
sam1-khan <samikhan15262822@gmail.com>
2024-11-01 12:52:12 +0500

    Add send_update_passwrd to tasks.py

--- attendance_project/attendance/tasks.py
+++ attendance_project/attendance/tasks.py
@@ -1,6 +1,7 @@
-import datetime
 from .models import Attendance
 from celery import shared_task
+from ..attendance_project import settings
+from django.template.defaultfilters import pluralize
 
 @shared_task
 def send_reminder():
@@ -24,5 +25,34 @@ def send_reminder():
             subject,
             message
         )
+    x = employees_to_remind.count()
+    s = pluralize(x)
+    return f'Sent Email{s} to {x} employees {s}'
 
-    return f'Sent Emails to {employees_to_remind.count()} employees'
+
+@shared_task
+def send_update_passwrd(user_id, passwrd):
+    new = settings.AUTH_USER_MODEL.get(id=user_id)
+    try:
+        subject = "Important: Set Up Your Password for Your New Account"
+        message = (
+            f"Hello {new.get_full_name() if new.first_name else new.get_username()},\n\n"
+            "Your HR has created an account for you on our attendance platform.\n\n"
+            "Login Details:\n"
+            f"Username: {new.get_username()}\n"
+            f"Temporary Password: {passwrd}\n\n"
+            "Next Steps:\n"
+            "1. Log in with the temporary password.\n"
+            "2. Go to “Account Settings” to set a new password.\n\n"
+            "Thank you!"
+        )
+
+        # Send email
+        new.email_user(
+            subject,
+            message
+        )
+        return f'Sent Email to new user: {user_id}'
+    except:
+        return f'Could not Send Email to new user: {user_id}'
+        
