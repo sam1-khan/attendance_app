e351f9417593b3bd30e0b76ea230b486b85d1527
8sami <samikhan15262822@gmail.com>
2024-11-04 01:55:55 +0500

    Implemented password reset functionality

--- attendance_project/attendance_project/urls.py
+++ attendance_project/attendance_project/urls.py
@@ -16,17 +16,23 @@ Including another URLconf
 import os
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.views.static import serve
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('attendance.urls')),  # Change to ads.urls
-    path('admin/', admin.site.urls),  # Keep
+    path('', include('attendance.urls')),  
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    re_path(r'^oauth/', include('social_django.urls', namespace='social')),  # Keep
+    
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset/password_reset_form.html', email_template_name='password_reset/password_reset_email.html', subject_template_name='password_reset/password_reset_subject.txt'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset/password_change.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'), name='password_reset_complete'),
 
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_reset/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset/password_change_done.html'), name='password_change_done'),
+    
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),  # Keep
 ]
 
 # Serve the favicon - Keep for later
