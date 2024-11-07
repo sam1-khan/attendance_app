6976f5f78dde0141de128116b7d140900668a6bf
8sami <samikhan15262822@gmail.com>
2024-11-07 15:28:54 +0500

    A lot of changes made in a lot files: added password change functionality, changes to ui & add alerts

--- attendance_project/attendance_project/settings.py
+++ attendance_project/attendance_project/settings.py
@@ -140,7 +140,7 @@ LOGIN_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'mail12.serv00.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
